b8e3ae07001f631c4402c3419b7bc97a9c2939eb
pedro-ms-br <pt971121@gmail.com>
2025-11-13 14:32:09 -0300

    v1.0 - em desenvolvimento, pagina 19

---
+++ app/src/main/java/com/pedroMoraes/kanban/ui/SplashFragment.kt
@@ -0,0 +1,31 @@
+package com.pedroMoraes.kanban.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pedroMoraes.kanban.R
+import com.pedroMoraes.kanban.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
