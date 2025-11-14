e54b045af001064ce5173520ea9cb1fc4bca7515
pedro-ms-br <pt971121@gmail.com>
2025-11-14 15:16:06 -0300

    v1.3.1 - em desenvolvimento, pré passo 64

--- app/src/main/java/com/pedroMoraes/kanban/ui/SplashFragment.kt
+++ app/src/main/java/com/pedroMoraes/kanban/ui/SplashFragment.kt
@@ -1,10 +1,14 @@
 package com.pedroMoraes.kanban.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.postDelayed
+import androidx.navigation.fragment.findNavController
 import com.pedroMoraes.kanban.R
 import com.pedroMoraes.kanban.databinding.FragmentSplashBinding
 
@@ -23,9 +27,20 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({checkAuth()}, 3000)
+    }
+
+    private fun checkAuth() {
+        findNavController().navigate(R.id.action_splashFragment_to_authentication)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 
 }
--- app/src/main/java/com/pedroMoraes/kanban/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/pedroMoraes/kanban/ui/auth/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.pedroMoraes.kanban.R
 import com.pedroMoraes.kanban.databinding.FragmentLoginBinding
 
@@ -23,6 +24,21 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
