2e509be1782468c3a321ca242f2ab0ec6ee48bab
pedro-ms-br <pt971121@gmail.com>
2025-11-14 15:24:44 -0300

    v1.3.2 - em desenvolvimento, pré passo 71

---
+++ app/src/main/java/com/pedroMoraes/kanban/ui/TodoFragment.kt
@@ -0,0 +1,22 @@
+package com.pedroMoraes.kanban.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pedroMoraes.kanban.R
+
+
+class TodoFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo, container, false)
+    }
+
+}
