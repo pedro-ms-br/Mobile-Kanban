469ec225f4fb40868f1da41e5cf64376d14bc33c
pedro-ms-br <pt971121@gmail.com>
2026-03-16 17:59:19 -0300

    v 1.5.1 - Passo 84, Parte 3

--- app/src/main/java/com/pedroMoraes/kanban/ui/HomeFragment.kt
+++ app/src/main/java/com/pedroMoraes/kanban/ui/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.pedroMoraes.kanban.R
 import com.pedroMoraes.kanban.databinding.FragmentHomeBinding
+import com.pedroMoraes.kanban.ui.adapter.ViewPagerAdapter
 
 class HomeFragment : Fragment() {
 
@@ -22,10 +24,30 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTabs() // inicia as tabs do view pager2
+    }
+
+    private fun initTabs() {
+
+        val pageAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pageAdapter
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_doing)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_done)
+
+        binding.viewPager.offscreenPageLimit = pageAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = getString(pageAdapter.getTitle(position))
+        }.attach()
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
+
