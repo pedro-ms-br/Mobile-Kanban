54c10c3dd857afcd94911aa14e9c9bfb73737a69
pedro-ms-br <pt971121@gmail.com>
2026-04-08 13:33:04 -0300

    v 1.10.1 - Passo 205, Parte 6!!!
    iniciando a mudanca do task adapter para diff util, com o objetivo de melhorar o desempenho do recycler view

--- app/src/main/java/com/pedroMoraes/kanban/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/pedroMoraes/kanban/ui/adapter/TaskAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.pedroMoraes.kanban.R
 import com.pedroMoraes.kanban.data.model.Status
@@ -15,7 +17,7 @@ class TaskAdapter (
     private val context: Context,
     private val taskList: List<Task>,
     private val taskSelected: (Task, Int) -> Unit
-): RecyclerView.Adapter<TaskAdapter.MyViewHolder> () {
+): ListAdapter<Task, TaskAdapter.MyViewHolder>(DIFF_CALLBACK) {
 
     companion object{
         val SELECT_BACK: Int = 1
@@ -23,6 +25,22 @@ class TaskAdapter (
         val SELECT_EDIT: Int = 3
         val SELECT_DETAILS: Int = 4
         val SELECT_NEXT: Int = 5
+
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Task>() {
+            override fun areItemsTheSame(
+                oldItem: Task,
+                newItem: Task
+            ): Boolean {
+                return oldItem.id == newItem.id && oldItem.description == newItem.description
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Task,
+                newItem: Task
+            ): Boolean {
+                return oldItem == newItem && oldItem.description == newItem.description
+            }
+        }
     }
 
     override fun onCreateViewHolder(
