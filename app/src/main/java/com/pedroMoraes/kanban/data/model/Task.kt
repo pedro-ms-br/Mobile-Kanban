57f12f0062512c6d383c2feffb09aebafc6c40cf
pedro-ms-br <pt971121@gmail.com>
2026-04-07 23:02:31 -0300

    v 1.9.4 - Passo 178, Parte 5
    criacao de adapters e data classes para mostrar os dados nas task items

---
+++ app/src/main/java/com/pedroMoraes/kanban/data/model/Task.kt
@@ -0,0 +1,7 @@
+package com.pedroMoraes.kanban.data.model
+
+data class Task (
+    val id: String,
+    val description: String
+
+)
---
+++ app/src/main/java/com/pedroMoraes/kanban/ui/adapter/TaskAdapter.kt
@@ -0,0 +1,32 @@
+package com.pedroMoraes.kanban.ui.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pedroMoraes.kanban.databinding.ItemTaskBinding
+
+class TaskAdapter: RecyclerView.Adapter<TaskAdapter.MyViewHolder> () {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: MyViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class MyViewHolder(val binding : ItemTaskBinding): RecyclerView.ViewHolder(binding.root) {
+        // cada item da lista
+
+    }
+
+}
