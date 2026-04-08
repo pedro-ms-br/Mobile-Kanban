932ba50952f3913d9ea2b533b6dee0a84f2ec2e0
pedro-ms-br <pt971121@gmail.com>
2026-04-07 23:14:02 -0300

    v 1.9.5 - Passo 182, Parte 5
    criacao de adapters e data classes para mostrar os dados nas task items

--- app/src/main/java/com/pedroMoraes/kanban/data/model/Task.kt
+++ app/src/main/java/com/pedroMoraes/kanban/data/model/Task.kt
@@ -1,7 +1,11 @@
 package com.pedroMoraes.kanban.data.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Task (
     val id: String,
     val description: String
 
-)
+):Parcelable
--- app/src/main/java/com/pedroMoraes/kanban/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/pedroMoraes/kanban/ui/adapter/TaskAdapter.kt
@@ -1,27 +1,32 @@
 package com.pedroMoraes.kanban.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.pedroMoraes.kanban.data.model.Task
 import com.pedroMoraes.kanban.databinding.ItemTaskBinding
 
-class TaskAdapter: RecyclerView.Adapter<TaskAdapter.MyViewHolder> () {
+class TaskAdapter (
+    private val taskList: List<Task>
+): RecyclerView.Adapter<TaskAdapter.MyViewHolder> () {
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): MyViewHolder {
-        TODO("Not yet implemented")
+        val view = ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(view)
     }
+
+    override fun getItemCount() = taskList.size
 
     override fun onBindViewHolder(
         holder: MyViewHolder,
         position: Int
     ) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        val task = taskList[position]
+        holder.binding.textDescription.text = task.description
     }
 
     inner class MyViewHolder(val binding : ItemTaskBinding): RecyclerView.ViewHolder(binding.root) {
