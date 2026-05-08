fb9e15d022da16ae5bc0ede88847187e2c07eb92
pedro-ms-br <pt971121@gmail.com>
2026-05-08 15:23:53 -0300

    v2.0.1 - Aula - implementando banco de dados
    TODO: Importar firebase

--- app/src/main/java/com/pedroMoraes/kanban/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/pedroMoraes/kanban/ui/auth/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.pedroMoraes.kanban.R
 import com.pedroMoraes.kanban.databinding.FragmentRegisterBinding
 
@@ -14,6 +16,10 @@ class RegisterFragment : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
 
+
+    //variavel da database
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,6 +28,37 @@ class RegisterFragment : Fragment() {
         return binding.root
     }
 
+
+    private fun validateData() {
+        val email = binding.editTextEmail.text().toString().trim()
+        val senha = binding.editTextSenha.text().toString.trim()
+
+        if (email.isNotBlank()) {
+            // navegar coisas
+            // findNavController().navigate(R.id.)
+        } else {
+            // coisas pra fazer se for vazio
+        }
+    }
+
+    private fun registerUser(email: String, senha: String) {
+        try {
+            // instanciando o auth
+            val auth = FirebaseAuth.getInstance()
+
+            // criando usuario no auth
+            auth.createUserWithEmailAndPAssword(email, senha)
+                .addCompleteListener{ task ->
+
+                }.addOnFailureListener{
+
+                }
+
+        } catch (e: Exception) {
+            Toast.makeText()
+        }
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
