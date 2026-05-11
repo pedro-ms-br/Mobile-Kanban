7208e2c7d0ad19431d606c8d952c04f02bbecb90
pedro-ms-br <pt971121@gmail.com>
2026-05-11 17:28:30 -0300

    v0.1 - dev logout -
    logica de logout finalizada, inicio da testagem
    
    TODO: arrumar estilo da pagina de criacao de conta

--- app/src/main/java/com/pedroMoraes/kanban/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/pedroMoraes/kanban/ui/auth/RegisterFragment.kt
@@ -32,7 +32,7 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initToolbar(binding.toolbar) // erro?
+        initToolbar(binding.toolbar)
         initListener()
     }
 
