12758982e0633120659d917a7ee8bf7356b8c88d
20232tiimi0322 <pt971121@gmail.com>
2026-05-18 17:19:43 -0300

    v1.1 - inicio do capitulo 10 -
    rushando, implementando sdk do firebase no build gradle
    
    TODO: arrumar estilo da pagina de criacao de conta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-analytics")
 }
